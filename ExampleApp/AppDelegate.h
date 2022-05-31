74a92374a3e0d0cf14510058ad28856b10d674c7
Steffen André Langnes <steffenl.sw@gmail.com>
2022-06-01 05:46:53 +0900

    Initial Commit

---
+++ ExampleApp/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ExampleApp
+//
+//  Created by Steffen on 2022/06/01.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ExampleApp/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  ExampleApp
+//
+//  Created by Steffen on 2022/06/01.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
